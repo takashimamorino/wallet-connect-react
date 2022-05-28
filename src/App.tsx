2042891f4deb0f6391c62d2d3eefde40f16a663f
takashimamorino <does1026@gmail.com>
2022-05-28 19:57:32 +0900

    feat: walletconnect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { providers } from "ethers";
+
+const handleClick = async () => {
+	const wcProvider = new WalletConnectProvider({
+		infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+		qrcodeModalOptions: {
+			mobileLinks: ["metamask"],
+			desktopLinks: ["metamask"],
+		},
+	});
+	await wcProvider.enable();
+	const provider = new providers.Web3Provider(wcProvider);
+	const signer = provider.getSigner();
+	console.log({ signer });
+};
+
 function App() {
-	return <h2>hello</h2>;
+	return (
+		<>
+			<button onClick={handleClick}>Connect</button>
+		</>
+	);
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	define: {
+		global: "globalThis",
+		"process.env": {},
+	},
+	plugins: [
+		react(),
+		NodeGlobalsPolyfillPlugin({
+			buffer: true,
+		}),
+	],
+	optimizeDeps: {
+		esbuildOptions: {
+			define: { global: "globalThis" },
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+			],
+		},
+	},
 });
